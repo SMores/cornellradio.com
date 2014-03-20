96b149c18d3311177189cd18b0009f204d365f4b
Shane Moore <shanewmoore@gmail.com>
2014-03-20 00:46:04 -0400

    Create app skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CornellRadio::Application.config.secret_key_base = 'f64f37efe623881b1ebbe60738ea748c64cc47ed76493f984429ba50988c22f5a89aef17a486336dd341b70f317b2b58f5b04fa124f1a742dd3e596ae3a7fddb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CornellRadio::Application.config.session_store :cookie_store, key: '_cornell-radio_session'
