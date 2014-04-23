c0053de96972834801bfe7156f59b75087c283e4
Shane Moore <shanewmoore@gmail.com>
2014-04-22 20:23:07 -0400

    Fix bug where users can't edit their own profiles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-  before_action :correct_access, only: [:new, :create, :destroy, :edit, :update]
+  before_action :correct_access, only: [:new, :create, :destroy]
   before_action :correct_user, only: [:edit, :update]
 
   # GET /users
