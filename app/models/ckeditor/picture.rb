62f96b1f19b1604980b685468cf253de193a1160
Shane Moore <shanewmoore@gmail.com>
2014-04-23 15:26:46 -0400

    Still trying to debug deploy issue

--- app/models/ckeditor/picture.rb
+++ app/models/ckeditor/picture.rb
@@ -2,10 +2,10 @@ class Ckeditor::Picture < Ckeditor::Asset
   has_attached_file :data,
                     :url  => "/ckeditor_assets/pictures/:id/:style_:basename.:extension",
                     :path => "/ckeditor_assets/pictures/:id/:style_:basename.:extension",
-                    storage: :s3,
-                    s3_credentials: "#{Rails.root}/config/aws.yml",
-                    :styles => { :content => '800>', :thumb => '118x100#' },
-                    bucket: 'cornell-radio-assets'
+                    # storage: :s3,
+                    # s3_credentials: "#{Rails.root}/config/aws.yml",
+                    :styles => { :content => '800>', :thumb => '118x100#' }
+                    # bucket: 'cornell-radio-assets'
 
   validates_attachment_size :data, :less_than => 2.megabytes
   validates_attachment_presence :data
--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,6 @@ Bundler.require(:default, Rails.env)
 module CornellRadio
   class Application < Rails::Application
 
-    config.assets.precompile += Ckeditor.assets
     config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
