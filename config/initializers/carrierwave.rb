0a0f3127c496fab20d096ee87b1df85b9c88f9ea
Shane Moore <shanewmoore@gmail.com>
2014-04-30 02:13:30 -0400

    Change max filesize for CarrierWave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,5 +4,6 @@ CarrierWave.configure do |config|
     :aws_access_key_id      => 'AKIAIB4YTLHWTO3NXLVA',                        # required
     :aws_secret_access_key  => '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',     # required
   }
+  config.max_file_size     = 100.megabytes  
   config.fog_directory  = 'cornell-radio-assets'                     # required
 end
